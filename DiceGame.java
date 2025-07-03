61c86bebf07e8cf284bcf61cf6159e42335d7e2c
YOKOHIRA Hazuki <yokohirahazuki0607@gmail.com>
2025-07-03 17:25:04 +0900

    Won/lost message added

---
+++ DiceGame.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+public class DiceGame{
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	Random rand = new Random();
+	int a = rand.nextInt(6)+1;
+	int b = rand.nextInt(6)+1;
+	int sum = a+b;
+
+	System.out.println("What is your name?");
+	String name = sc.nextLine();
+	System.out.println("Helleo, "+name+"!");
+
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1: "+a);
+	System.out.println("Die 2: "+b);
+	System.out.println("Total value: "+sum);
+
+	if(sum>7) System.out.println(name+" won!");
+	else System.out.println(name+" lost...");
+    }
+}
